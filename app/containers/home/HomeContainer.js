06d8fda0d9e09395583ea4a4377394dabd1e266f
Joe Dahle <joe.dahle@gmail.com>
2017-10-28 23:03:35 -0700

    hello world

---
+++ app/containers/home/HomeContainer.js
@@ -0,0 +1,33 @@
+// Core
+import React, { Component } from 'react';
+import PropTypes from 'react';
+import { Link } from 'react-router';
+
+// components
+
+
+// styles
+// import './_index.scss';
+
+// Data
+
+// var HomeContainer = React.createClass({
+//   render: function() {
+//     return (
+//       <div>Hello World</div>
+//     );
+//   }
+// });
+
+class HomeContainer extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>Hello World</div>
+    )
+  }
+}
+
+export default HomeContainer;
